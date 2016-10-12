1def9435406d4f84bb67b5a220a9f8802c656b7c
karatechops <contact@lazers.tv>
2016-10-12 11:54:23 -0400

    Updated illustrations.

--- src/js/sections/End.js
+++ src/js/sections/End.js
@@ -5,13 +5,16 @@ import EndButton from '../components/EndButton';
 import Box from 'grommet/components/Box';
 import Headline from 'grommet/components/Headline';
 import Button from 'grommet/components/Button';
+import Group from 'grommet/components/icons/base/Group';
 
 export default function End(props) {
   return (
     <InfographicSection className="infographic__section infographic__section--end" 
       colorIndex="light-2">
       <Box direction="row" justify="center" align="center">
-        <img src="../img/end/millennials.svg" />
+        <Box pad="large" alignSelf="start">
+          <Group size="huge" colorIndex="neutral-2" />
+        </Box>
         <Box>
           <Headline size="large">These are just some of the trends that
             will shape the new Millennial economy.</Headline>
