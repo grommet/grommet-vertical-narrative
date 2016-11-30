8f955c244f829e461c9e8b5c1e4127f6f8365673
karatechops <contact@lazers.tv>
2016-11-30 17:45:43 -0500

    Update build config.

--- src/js/App.js
+++ src/js/App.js
@@ -3,7 +3,6 @@
 import React from 'react';
 import GrommetApp from 'grommet/components/App';
 import HomePage from './components/Home';
-
 import '../scss/index.scss';
 
 class App extends React.Component {
