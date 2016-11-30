4b331146d1f764eb44ec49da24f99e89b8a6be2d
karatechops <contact@lazers.tv>
2016-11-30 17:45:43 -0500

    Update build config.

--- grommet-toolbox.config.js
+++ grommet-toolbox.config.js
@@ -7,7 +7,6 @@ export default {
   dist: path.resolve(__dirname, 'dist/'),
   copyAssets: [
     'src/index.html',
-    'src/robots.txt',
     {
       asset: 'src/develop/img/**',
       dist: 'dist/img/'
@@ -50,7 +49,33 @@ export default {
       root: [
         path.resolve(__dirname, './node_modules')
       ]
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.jsx?$/,
+          loader: 'babel-loader',
+          include: /node_modules\/hpe-digitaltoolkit/
+        },
+        {
+          test: /\.svg$/,
+          loader: 'url-loader?limit=10000'
+        }
+      ]
     }
+  },
+  scssLoader: {
+    test: /\.scss$/,
+    loader: "file?name=assets/css/[name].css!sass?" +
+      'includePaths[]=' +
+      (encodeURIComponent(
+        path.resolve(process.cwd(), './node_modules')
+      )) +
+      '&includePaths[]=' +
+      (encodeURIComponent(
+        path.resolve(process.cwd(),
+        './node_modules/grommet/node_modules'))
+      )
   },
   devServerPort: 8002,
   //devServerHost: "10.0.0.1",
@@ -61,9 +86,6 @@ export default {
     'grommet/scss': path.resolve(__dirname, '../grommet/src/scss'),
     'grommet': path.resolve(__dirname, '../grommet/src/js')
   },
-  devPreprocess: [
-    'set-webpack-alias', 'dist-css', 'watch-css'
-  ],
-  distPreprocess: ['set-webpack-alias', 'dist-css', 
-    'generate-server-routes']
+  devPreprocess: ['set-webpack-alias'],
+  distPreprocess: ['set-webpack-alias']
 };
