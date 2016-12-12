b822b32b5e012c27de2c3945f22de30587a17ce6
karatechops <contact@lazers.tv>
2016-12-12 15:29:49 -0500

    Remove extraneous dependencies.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -15,14 +15,4 @@ gulp.task('set-webpack-alias', function () {
   }
 });
 
-
-var nodeModules = {};
-fs.readdirSync('node_modules')
-  .filter(function(x) {
-    return ['.bin'].indexOf(x) === -1;
-  })
-  .forEach(function(mod) {
-    nodeModules[mod] = 'commonjs ' + mod;
-  });
-
 grommetToolbox(gulp, options);
